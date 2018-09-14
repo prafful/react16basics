306e7a838a49ff737a8c1225cf7a6cbdf823f38a
prafful <prraful@gmail.com>
2018-09-14 16:41:18 +0530

    react cordova (not working) and spa in react

---
+++ spareact/src/SPAWebsiteComponent/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  
+    render() { 
+        return ( 
+            <div>
+            Page not found. Pl. go to the home page or get in touch with web master.
+            </div>
+         );
+    }
+}
+ 
+export default NotFound;
--- spareact/src/SPAWebsiteComponent/SpaWebsite.js
+++ spareact/src/SPAWebsiteComponent/SpaWebsite.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import { HashRouter, NavLink, Route } from "react-router-dom";
+import { HashRouter, NavLink, Route, BrowserRouter, Redirect } from "react-router-dom";
 import Home from './Home'
 import About from './About'
 import Friends from './Friends'
+import NotFound from './NotFound'
 
 class SpaWebsite extends React.Component {
    
     render() { 
         return ( 
-            <HashRouter>
+            <BrowserRouter>
                 <div>
                     <h1>SPA in React</h1>
                     <ul>
-                        <li><NavLink exact  to="/">Home</NavLink></li>
-                        <li><NavLink to="/about">About</NavLink></li>
-                        <li><NavLink to="/friends">Friends</NavLink></li>
+                        <li><NavLink exact to="/">Home</NavLink></li>
+                        <li><NavLink  to="/about">About</NavLink></li>
+                        <li><NavLink   to="/friends" >Friends</NavLink></li>
                     </ul>
                     <div>
+                        
                         <Route exact path="/" component={Home}/>
-                        <Route path="/about" component={About}/>
-                        <Route path="/friends" component={Friends}/>
-
+                        <Route  path="/about" component={About}/>
+                        <Route   path="/friends" component={Friends} />
+                        <Redirect to="/" />
                     </div>
 
                 </div>
-            </HashRouter>
+            </BrowserRouter>
          );
     }
 }
